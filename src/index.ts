495fe23449c58d771ba9f2ad9bf891f0cc750f0a
LuboDimitrov <lubo9991@hotmail.com>
2024-08-05 16:20:25 +0200

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import 'dotenv/config'
 import router from "./routes";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors())
 app.use('/api', router)
 
 export default app;
